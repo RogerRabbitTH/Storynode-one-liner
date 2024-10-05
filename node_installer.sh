d5e52ee41a618202c5f15ce1df94ddca039bf88d
RogerRabbit <rogertodamoon1@gmail.com>
2024-10-05 13:25:20 +0700

    add LIVE_PEER

--- node_installer.sh
+++ node_installer.sh
@@ -86,6 +86,7 @@ story version
 read -p "Enter your moniker name: " moniker_name
 story init --network iliad --moniker "${moniker_name}"
 echo -e "${BOLDGREEN}Your moniker name is: ${YELLOW}${moniker_name}${ENDCOLOR}"
+
 sleep 2
 
 echo -e "${BOLDCYAN}Creating story-geth service${ENDCOLOR}"
@@ -125,7 +126,19 @@ LimitNOFILE=4096
 WantedBy=multi-user.target
 EOF
 
+echo -e "${BOLDYELLOW}Updating persistent_peers${ENDCOLOR}"
+sleep 1
+
+PEERS=$(curl -s -X POST https://rpc-story.josephtran.xyz -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","method":"net_info","params":[],"id":1}' | jq -r '.result.peers[] | select(.connection_status.SendMonitor.Active == true) | "\(.node_info.id)@\(if .node_info.listen_addr | contains("0.0.0.0") then .remote_ip + ":" + (.node_info.listen_addr | sub("tcp://0.0.0.0:"; "")) else .node_info.listen_addr | sub("tcp://"; "") end)"' | tr '\n' ',' | sed 's/,$//' | awk '{print "\"" $0 "\""}')
+sed -i "s/^persistent_peers *=.*/persistent_peers = $PEERS/" "$HOME/.story/story/config/config.toml"
+    if [ $? -eq 0 ]; then
+echo -e "Configuration file updated successfully with new peers"
+    else
+echo "Failed to update configuration file."
+fi
+
 echo -e "${BOLDGREEN}Starting services${ENDCOLOR}"
+sleep 1
 
 sudo systemctl daemon-reload && \
 sudo systemctl start story-geth && \
