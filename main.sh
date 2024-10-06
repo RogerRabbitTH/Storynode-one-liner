587832c5dd3d5f678eb6508ebb17ccdba1b8a04e
RogerRabbit <rogertodamoon1@gmail.com>
2024-10-06 13:08:17 +0700

    change main.sh to curl

--- main.sh
+++ main.sh
@@ -26,19 +26,19 @@ handle_main_choice() {
     case $1 in
         1)
             echo -e "${BOLDGREEN}Node Install...${ENDCOLOR}"
-            ./node_installer.sh
+            curl -O https://raw.githubusercontent.com/RogerRabbitTH/Storynode-one-liner/main/node_installer.sh && chmod +x node_installer.sh && ./node_installer.sh
             ;;
         2)
             echo -e "${BOLDGREEN}Snapshot Install...${ENDCOLOR}"
-            ./snapshot_installer.sh
+            curl -O https://raw.githubusercontent.com/RogerRabbitTH/Storynode-one-liner/main/snapshot_installer.sh && chmod +x snapshot_installer.sh && ./snapshot_installer.sh
             ;;
         3)
             echo -e "${BOLDGREEN}Update Node...${ENDCOLOR}"
-            ./update_node.sh
+            curl -O https://raw.githubusercontent.com/RogerRabbitTH/Storynode-one-liner/main/update_node.sh && chmod +x update_node.sh && ./update_node.sh
             ;;
         4)
             echo -e "${BOLDGREEN}Node Status...${ENDCOLOR}"
-            ./nodeStatus.sh
+            curl -O https://raw.githubusercontent.com/RogerRabbitTH/Storynode-one-liner/main/nodeStatus.sh && chmod +x nodeStatus.sh && ./nodeStatus.sh
             ;;
         5)
             echo -e "${BOLDYELLOW}Exiting...${ENDCOLOR}"
