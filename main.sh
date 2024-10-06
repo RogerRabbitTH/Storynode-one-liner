a85e7df4559b55c1ee5c128c56976d5fa7d57f4c
RogerRabbit <rogertodamoon1@gmail.com>
2024-10-06 13:24:25 +0700

    curl -s -O

--- main.sh
+++ main.sh
@@ -24,19 +24,19 @@ handle_main_choice() {
     case $1 in
         1)
             echo -e "${BOLDGREEN}Node Install...${ENDCOLOR}"
-            curl -O https://raw.githubusercontent.com/RogerRabbitTH/Storynode-one-liner/main/node_installer.sh && chmod +x node_installer.sh && ./node_installer.sh
+            curl -s -O https://raw.githubusercontent.com/RogerRabbitTH/Storynode-one-liner/main/node_installer.sh && chmod +x node_installer.sh && ./node_installer.sh
             ;;
         2)
             echo -e "${BOLDGREEN}Snapshot Install...${ENDCOLOR}"
-            curl -O https://raw.githubusercontent.com/RogerRabbitTH/Storynode-one-liner/main/snapshot_installer.sh && chmod +x snapshot_installer.sh && ./snapshot_installer.sh
+            curl -s -O https://raw.githubusercontent.com/RogerRabbitTH/Storynode-one-liner/main/snapshot_installer.sh && chmod +x snapshot_installer.sh && ./snapshot_installer.sh
             ;;
         3)
             echo -e "${BOLDGREEN}Update Node...${ENDCOLOR}"
-            curl -O https://raw.githubusercontent.com/RogerRabbitTH/Storynode-one-liner/main/update_node.sh && chmod +x update_node.sh && ./update_node.sh
+            curl -s -O https://raw.githubusercontent.com/RogerRabbitTH/Storynode-one-liner/main/update_node.sh && chmod +x update_node.sh && ./update_node.sh
             ;;
         4)
             echo -e "${BOLDGREEN}Node Status...${ENDCOLOR}"
-            curl -O https://raw.githubusercontent.com/RogerRabbitTH/Storynode-one-liner/main/nodeStatus.sh && chmod +x nodeStatus.sh && ./nodeStatus.sh
+            curl -s -O https://raw.githubusercontent.com/RogerRabbitTH/Storynode-one-liner/main/nodeStatus.sh && chmod +x nodeStatus.sh && ./nodeStatus.sh
             ;;
         5)
             echo -e "${BOLDYELLOW}Exiting...${ENDCOLOR}"
